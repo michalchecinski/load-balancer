ec9aa2ccaf37f0b1201d780d7737ca6c616c5b55
Michał Chęciński <michal.checinski@outlook.com>
2019-05-09 15:21:15 +0200

    Add settings form json adding to config

--- Projekt/LoadBalancer/LoadBalancer/Startup.cs
+++ Projekt/LoadBalancer/LoadBalancer/Startup.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LoadBalancer.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LoadBalancer
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            string[] files = Directory.GetFiles(Path.Combine(env.ContentRootPath, "settings"));
+
+            foreach (var s in files)
+            {
+                builder = builder.AddJsonFile(s);
+            }
+
+            builder = builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            var config = new LoadBalancerSettings();
+            Configuration.Bind("BalancerSettings", config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +51,14 @@ namespace LoadBalancer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.Use((context, next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                
+                
+                return next();
             });
+
+                        
         }
     }
 }
