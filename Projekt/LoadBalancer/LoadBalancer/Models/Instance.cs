44169e4b4d0536e924385997411565270d8ec9cc
Michał Chęciński <michal.checinski@outlook.com>
2019-05-09 15:22:24 +0200

    Add settings binding classes

---
+++ Projekt/LoadBalancer/LoadBalancer/Models/Instance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer.Models
+{
+    public class Instance
+    {
+        public string Ip { get; set; }
+        public string Port { get; set; }
+    }
+
+}
