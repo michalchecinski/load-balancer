077ca3adb7d019e434c9d895964b2c237b5825e7
Michał Chęciński <michal.checinski@outlook.com>
2019-05-09 16:20:25 +0200

    Add middleware extensions for simpler use in startup

---
+++ Projekt/LoadBalancer/LoadBalancer/MiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using LoadBalancer.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBalancer(
+           this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BalancingMiddleware>();
+        }
+    }
+}
