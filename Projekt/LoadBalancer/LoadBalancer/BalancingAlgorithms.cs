10b2ba1d5b47d189cb7562a0da0a83e3a663fdb9
Michał Chęciński <michal.checinski@outlook.com>
2019-05-09 16:38:53 +0200

    Add load ballancing alorithms class with random implementation

---
+++ Projekt/LoadBalancer/LoadBalancer/BalancingAlgorithms.cs
@@ -0,0 +1,20 @@
+﻿using LoadBalancer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer
+{
+    public class BalancingAlgorithms
+    {
+        public static Instance Random()
+        {
+            var instances = LoadBalancerSettings.Current.Instances;
+            Random random = new Random();
+            var index = random.Next(0, instances.Length);
+            return instances[index];
+        }
+    }
+}
