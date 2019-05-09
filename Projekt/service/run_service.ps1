42b2e1adac8c128ab07dbef451086b71e9069960
Michał Chęciński <michal.checinski@outlook.com>
2019-05-09 13:27:01 +0200

    Change run script to run 2 instances

--- Projekt/service/run_service.ps1
+++ Projekt/service/run_service.ps1
@@ -1,1 +1,5 @@
-docker run -p 5000:5000 -e INSTANCE='1' -v $PWD/src:/src service
+docker kill $(docker ps -q)
+
+docker run -p 5000:5000 -e INSTANCE='1' -d -v $PWD/src:/src service
+
+docker run -p 5001:5000 -e INSTANCE='2' -d -v $PWD/src:/src service
