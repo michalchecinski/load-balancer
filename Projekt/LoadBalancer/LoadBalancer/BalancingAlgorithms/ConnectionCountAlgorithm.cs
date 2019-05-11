3ac104c29a488f8a4f281de696b60af69760cc0e
Michał Chęciński <michal.checinski@outlook.com>
2019-05-11 11:13:19 +0200

    Add balancing algorithms as implementation of abstract class

---
+++ Projekt/LoadBalancer/LoadBalancer/BalancingAlgorithms/ConnectionCountAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoadBalancer.Models;
+
+namespace LoadBalancer.BalancingAlgorithms
+{
+    public class ConnectionCountAlgorithm : BalancingAlgorithm
+    {
+        private static Dictionary<Instance, long> instanceUsage = new Dictionary<Instance, long>();
+
+        public ConnectionCountAlgorithm()
+        {
+            SetInstancesUsageToZero();
+        }
+
+        public override Instance GetInstance()
+        {
+            var instance = instanceUsage.OrderBy(x => x.Value).FirstOrDefault().Key;
+
+            if (instanceUsage[instance] + 1 > long.MaxValue)
+            {
+                SetInstancesUsageToZero();
+            }
+            instanceUsage[instance]++;
+
+            return instance;
+        }
+
+        private void SetInstancesUsageToZero()
+        {
+            foreach (var item in instances)
+            {
+                instanceUsage[item] = 0;
+            }
+        }
+    }
+}
