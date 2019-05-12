f1921260e76ed0a8ca78f926f8185fa85ca3bc3a
Michał Chęciński <michal.checinski@outlook.com>
2019-05-12 21:41:15 +0200

    Add web project to show logs

---
+++ Projekt/LoadBalancer/LoadBalancer.Logs.Web/Controllers/LogsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoadBalancer.Logs;
+
+namespace LoadBalancer.Logs.Web.Controllers
+{
+    public class LogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Logger.instance.GetLogs());
+        }
+    }
+}
