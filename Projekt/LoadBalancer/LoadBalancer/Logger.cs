2dad90338703b5558f115aa88d99d8b8c3eab8b8
Michał Chęciński <michal.checinski@outlook.com>
2019-05-12 20:48:18 +0200

    Add logger class

---
+++ Projekt/LoadBalancer/LoadBalancer/Logger.cs
@@ -0,0 +1,24 @@
+﻿using LoadBalancer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer
+{
+    public class Logger
+    {
+        private static List<LoggingEntity> loggingEntities = new List<LoggingEntity>();
+
+        public static void Log(LoggingEntity loggingEntity)
+        {
+            loggingEntities.Add(loggingEntity);
+        }
+
+        public static void Log(string requestUrl, string method, Instance instance, string requestToInstanceUrl, int responseStatusCode)
+        {
+            Log(new LoggingEntity(requestUrl, method, instance, requestToInstanceUrl, responseStatusCode));
+        }
+    }
+}
