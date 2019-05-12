ae8cb4af5abfb777a219bceac1f53409e2baa930
Michał Chęciński <michal.checinski@outlook.com>
2019-05-12 20:48:06 +0200

    Add logging entity model

---
+++ Projekt/LoadBalancer/LoadBalancer/Models/LoggingEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+
+namespace LoadBalancer.Models
+{
+    public class LoggingEntity
+    {
+        public DateTime Time { get; set; }
+        public string RequestUrl { get; set; }
+        public string Method { get; set; }
+        public Instance Instance { get; set; }
+        public string RequestToInstanceUrl { get; set; }
+        public int ResponseStatusCode { get; set; }
+
+        public LoggingEntity(string requestUrl, string method, Instance instance, string requestToInstanceUrl, int responseStatusCode)
+        {
+            Time = DateTime.UtcNow;
+            RequestUrl = requestUrl;
+            Method = method;
+            Instance = instance;
+            RequestToInstanceUrl = requestToInstanceUrl;
+            ResponseStatusCode = responseStatusCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{Time} {RequestUrl} {Method} {Instance.Ip}:{Instance.Port} {RequestToInstanceUrl} {ResponseStatusCode}";
+        }
+    }
+}
